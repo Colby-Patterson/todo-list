e14b1ca6d39e01044aa120b6c6d4aa06df66c7d7
Colby Patterson <cpatt@byui.edu>
2022-07-14 10:21:39 -0600

    Todo list working

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { Button, Container, Divider, Form, Header, Input, List, Segment } from "semantic-ui-react";
 
 export default class TodoList extends React.Component {
-  render () {
+  state = { name: '', todos: ["work", "sleep"] }
+  handleSubmit = () => {
+    let newTodos = [this.state.name, ...this.state.todos]
+    this.setState({
+      todos: newTodos,
+      name: ''
+    })
+  }
+  render() {
     return (
-      <div>
-        <h1>TodoList</h1>
-      </div>
+      <Container>
+
+        <Segment>
+          <Header as='h1'>TodoList</Header>
+          <Form onSubmit={this.handleSubmit}>
+            <Input label='Todo name'
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })} />
+            <Divider />
+            <Button type='submit' color="blue">Add</Button>
+          </Form>
+          <Divider />
+          <List>
+            {this.state.todos.map(t => <List.Item key={t}>{t}</List.Item>)}
+          </List>
+        </Segment>
+      </Container>
     )
   }
 }
